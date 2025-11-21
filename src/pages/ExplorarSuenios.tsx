ce59988bc8d9aec7cf0594123207adce1f5a44b7
Marcos Diaz <ma.nahuel.d@gmail.com>
2025-11-21 02:05:54 -0300

    Se crea la vista para los nodos de sueños

--- src/pages/ExplorarSuenios.tsx
+++ src/pages/ExplorarSuenios.tsx
@@ -1,5 +1,25 @@
+import GrafoSuenios from "../components/GrafoSuenios";
+import { useEffect, useState } from "react";
+
 export default function ExplorarSuenios() {
-    return(
-        <div></div>        
-    );
+  const [suenios, setSuenios] = useState([]);
+  const [conexiones, setConexiones] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:8080/sueño`)
+      .then((res) => res.json())
+      .then(setSuenios);
+
+    fetch(`http://localhost:8080/sueño/conexiones`)
+      .then((res) => res.json())
+      .then(setConexiones);
+  }, []);
+
+  if (suenios.length === 0) return <p className="text-center">Cargando sueños...</p>;
+
+  return (
+    <>
+      <GrafoSuenios suenios={suenios} conexiones={conexiones} />
+    </>
+  );
 }
