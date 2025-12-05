56e870685135a069ee76fed566a89415a9c3b49d
Marcos Diaz <ma.nahuel.d@gmail.com>
2025-12-05 14:53:49 -0300

    Pancho tené buen finde

--- src/firebaseConfig.tsx
+++ src/firebaseConfig.tsx
@@ -1,4 +1,3 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore, Firestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
@@ -13,16 +12,6 @@ const firebaseConfig = {
   messagingSenderId: "264484217898",
   appId: "1:264484217898:web:25605c7a57cb5e61becf53"
 };
-
- /* EPERS test de Lucio
-  // apiKey: "AIzaSyD5swvDolS_mr1jfdwocr8WwEUQDRS0L7g",
-  // authDomain: "epersomniatest.firebaseapp.com",
-  // projectId: "epersomniatest",
-  // storageBucket: "epersomniatest.firebasestorage.app",
-  // messagingSenderId: "319854078662",
-  // appId: "1:319854078662:web:8e54222bb0f9087fbe45c7",
-  // measurementId: "G-3JWR8LLGLJ"
-  */
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
